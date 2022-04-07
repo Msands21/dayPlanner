edbe30fb4b00ef738151fe8cf454768e0361471b
Mike Sands <mikesands94@gmail.com>
2022-04-07 18:25:42 -0400

    button clicks save to local storage at all hours of day

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,14 @@
 //moment.js today's date 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+
+//save inputs to local storage upon save button click 
+function saveToLocalStorage(event) {
+    var hour = event.target.dataset.hour;
+    var valueOfText = $("#text-"+hour).val();
+
+    localStorage.setItem(hour, valueOfText)
+}
+
+$(document).on("click", "button", saveToLocalStorage)
